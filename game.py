e7c6082f736905d9928c9d0dc381f2504e94975a
pyPrarthan <christianprarthan07@gmail.com>
2024-08-27 18:46:36 -0400

    generate_code func added

--- game.py
+++ game.py
@@ -1,10 +1,20 @@
 # Importing random for generating random code
 import random
+from re import L
 
-COLOS = ["R", "G", "B", "Y", "W", "O"]
+COLORS = ["R", "G", "B", "Y", "W", "O"]
 TRIES = 10
 CODE_LENGTH = 4
 
 # Generate a random code of length CODE_LENGTH using random.choice
 def generate_code():
-    pass
+    code = []
+
+    for _ in range(CODE_LENGTH):
+        color = random.choice(COLORS)
+        code.append(color)
+    return code 
+
+
+code = generate_code()
+print(code)
