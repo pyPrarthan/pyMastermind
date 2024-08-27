30a71a473a0f1aee8dc148a697893046bf9b7dd6
pyPrarthan <christianprarthan07@gmail.com>
2024-08-27 18:42:16 -0400

    Project Started

---
+++ game.py
@@ -0,0 +1,10 @@
+# Importing random for generating random code
+import random
+
+COLOS = ["R", "G", "B", "Y", "W", "O"]
+TRIES = 10
+CODE_LENGTH = 4
+
+# Generate a random code of length CODE_LENGTH using random.choice
+def generate_code():
+    pass
